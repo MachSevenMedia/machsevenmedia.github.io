de8ca2de6c421354d1f9b0a96eeb0155d1ad07b7
Mark E. Chiles <mchiles@hartenergy.com>
2014-12-16 19:45:28 -0600

    symcat.html

--- app.js
+++
@@ -1,8 +0,0 @@
-var myApp = angular.module('myApp',[]);
-
-function MyCtrl($scope, $timeout) {
-  $scope.frameUrl = 'http://www.symcat.com/';
-  $scope.frameName = 'Symptom Checker';
-  $timeout(function () {
-  }, 1000)
-};
