954e5db4f83ef72864713213afbe4273938dc289
Derick Baker <dbaker@hartenergy.com>
2015-05-08 09:41:04 -0500

    New design

--- js/randbg.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
- $randomNumber = Math.floor((Math.random()*4)+1);
- $bg="img/bg"+$randomNumber+".jpg";
- $("#large-header").css({'background-image': 'url(' + $bg + ')', });
-});	// End document.ready()
--- js/script2.js
+++
@@ -1,9 +0,0 @@
-(function() {
-
-$(window).resize(function() {
-            var sH = $(window).height();
-            $('.masthead').css('height', (sH - $('header').outerHeight()) + 'px');
-           // $('section:not(.header-10-sub):not(.content-11)').css('height', sH + 'px');
-        });  
-    
-})();
